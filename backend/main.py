f182d12f46bbfad264838d249f9c51408a3d8f2b
Lunatic-A <ayesha367ans@gmail.com>
2026-07-25 17:47:02 +0530

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
         "http://localhost:5173",
         "lead-desk-mini-gvm2mo3t3-ashprojectsvercelapp.vercel.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
